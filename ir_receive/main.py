4dfd8b9596574714c47fe9af92bf4d3dd01e5126
Stuart McHattie <stuart.mchattie@gmail.com>
2021-12-20 11:13:00 +0000

    Add project that detects IR signals

---
+++ ir_receive/main.py
@@ -0,0 +1,16 @@
+import time
+from machine import Pin
+from ir_rx.nec import NEC_8  # NEC remote, 8 bit addresses.
+
+def ir_received(data, addr, ctrl):
+    if data >= 0:  # Not a repeat of the previous code.
+        # For now, just print what was received,
+        # but can trigger whatever action we want from here.
+        print('Data {:02x} Addr {:04x}'.format(data, addr))
+
+# Data leg from IR receiver on GPIO pin 22 (physical pin 29).
+ir = NEC_8(Pin(22, Pin.IN), ir_received)
+
+while True:
+    # Do nothing while we're waiting for IR signals.
+    pass
